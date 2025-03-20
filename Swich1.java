00fc55f5f50e00b1e557a7629681ddda63cf5b42
Beatriz Kailane <beatrizkailany3@gmail.com>
2025-03-20 09:55:21 -0300

    Update Swich1.java

--- Swich1.java
+++ Swich1.java
@@ -6,46 +6,48 @@ public class Swich1 {
 
 	public static void main(String[] args) {
 	
-		Scanner lerScannner = new Scanner (System.in);
-		
-		
-		    System.out.println("####################################################");
-	        System.out.println("Digite 1 para Cachorro-quente            - R$10,00");
-	        System.out.println("Digite 2 para X-Salada        		 - R$15,00");
-	        System.out.println("Digite 3 para X-Bacon         		 - R$18,00");
-	        System.out.println("Digite 4 para Bauru         	         - R$12,00");
-	        System.out.println("Digite 5 para Refrigerante    		 - R$8,00");
-	        System.out.println("Digite 6 para Suco            		 - R$3,00");
-	        System.out.println("###################################################");
+	Scanner lerScannner = new Scanner (System.in);		
+	int cod;
+        int preco = 0;
+        String produto = "";
 
-	        System.out.print("Informe o código do produto: ");
-	        int cod = lerScannner.nextInt();
-	    	lerScannner.nextLine();
+        System.out.println("####################################################");
+        System.out.println("Digite 1 para Cachorro-quente            - R$10,00");
+        System.out.println("Digite 2 para X-Salada                   - R$15,00");
+        System.out.println("Digite 3 para X-Bacon                     - R$18,00");
+        System.out.println("Digite 4 para Bauru                       - R$12,00");
+        System.out.println("Digite 5 para Refrigerante                - R$8,00");
+        System.out.println("Digite 6 para Suco                        - R$3,00");
+        System.out.println("###################################################");
 
-	        System.out.print("Informe a quantidade desejada: ");
-	        int quantidade = lerScannner.nextInt();
+        while (true) {
+            System.out.print("Informe o código do produto: ");
+            cod = lerScannner.nextInt();
+            lerScannner.nextLine(); 
+            
+            if (cod >= 1 && cod <= 6) {
+                break; 
+            }
+            System.out.println("Opção inválida! Digite um código entre 1 e 6.");
+        }
 
-	        int preco = 0;
-	        String produto = "";
+        System.out.print("Informe a quantidade desejada: ");
+        int quantidade = lerScannner.nextInt();
 
-	        switch (cod) {
-	            case 1: produto = "Cachorro-quente"; preco = 10; break;
-	            case 2: produto = "X-Salada"; preco = 15; break;
-	            case 3: produto = "X-Bacon"; preco = 18; break;
-	            case 4: produto = "Bauru"; preco = 12; break;
-	            case 5: produto = "Refrigerante"; preco = 8; break;
-	            case 6: produto = "Suco"; preco = 3; break;
-	            default:
-	                System.out.println("Opção inválida!");
-	                lerScannner.close();
-	                
-	                return;
-	        }
+        switch (cod) {
+            case 1: produto = "Cachorro-quente"; preco = 10; break;
+            case 2: produto = "X-Salada"; preco = 15; break;
+            case 3: produto = "X-Bacon"; preco = 18; break;
+            case 4: produto = "Bauru"; preco = 12; break;
+            case 5: produto = "Refrigerante"; preco = 8; break;
+            case 6: produto = "Suco"; preco = 3; break;
+        }
 
-	        int total = quantidade * preco;
-	        System.out.printf("O valor total de %d %s(s) é de R$%d,00%n", quantidade, produto, total);
+        int total = quantidade * preco;
+        System.out.printf("O valor total de %d %s(s) é de R$%d,00%n", quantidade, produto, total);
 
+       lerScannner.close();
 	      
-			}
+    }
 }
 
